4b08c66b7036dd9bbb79853af314212ac88eeffc
RyujiIto <ryuji0222ss@gmail.com>
2020-12-23 16:48:17 +0900

    :heavy_plus_sign: add snake_line
    add snake_line

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import hashlib
 import operator
 import time
 from collections import Counter
+import operator
 
 
 def bubble_sort(numbers: List[int]) -> List[int]:
@@ -331,9 +332,54 @@ def list_to_int_plus_one(numbers: List[int]) -> int:
     return list_to_int(numbers)
 
 
+def snake_string_v1(chars: str) -> List[List[str]]:
+    result = [[], [], []]
+    result_indexes = {0, 1, 2}
+    insert_index = 1
+    for i, s in enumerate(chars):
+        if i % 4 == 1:
+            insert_index = 0
+        elif i % 2 == 0:
+            insert_index = 1
+        elif i % 4 == 3:
+            insert_index = 2
+        result[insert_index].append(s)
+        for rest_index in result_indexes - {insert_index}:
+            result[rest_index].append(" ")
+    return result
+
+
+def snake_string_v2(chars: str, depth: int) -> List[List[str]]:
+    result = [[] for _ in range(depth)]
+    result_indexes = {i for i in range(depth)}
+    insert_index = int(depth / 2)
+
+    # def pos(i):
+    #     return i + 1
+
+    # def neg(i):
+    #     return i - 1
+    op = operator.neg
+
+    for s in chars:
+        result[insert_index].append(s)
+        for rest_index in result_indexes - {insert_index}:
+            result[rest_index].append(" ")
+        if insert_index <= 0:
+            op = operator.pos
+        if insert_index >= depth - 1:
+            op = operator.neg
+        insert_index += op(1)
+    return result
+
+
 if __name__ == '__main__':
-    l = [1, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5]
-    l_counter = Counter(l)
-    print(l_counter)
-    for i, v in l_counter.items():
-        print(i, v)
+    # numbers = [str(i) for j in range(5) for i in range(10)]
+    # strings = "".join(numbers)
+    # for line in snake_string_v1(strings):
+    #     print("".join(line))
+    import string
+    alphabet = [s for _ in range(2) for s in string.ascii_lowercase]
+    strings = "".join(alphabet)
+    for line in snake_string_v2(strings, 2):
+        print("".join(line))
