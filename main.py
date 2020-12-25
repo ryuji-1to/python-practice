16593d3345cf240870e3fed993b80decf24082f5
RyujiIto <ryuji0222ss@gmail.com>
2020-12-25 20:31:56 +0900

    :heavy_plus_sign: add func
    add func

--- main.py
+++ main.py
@@ -486,7 +486,28 @@ def find__all_palindrome(strings: str):
         yield from find_palindrome(strings, i - 1, i)
 
 
+def order_even_odd_last(numbers: List[int]) -> None:
+    even_list = []
+    odd_list = []
+    for num in numbers:
+        if num % 2 == 0:
+            even_list.append(num)
+        else:
+            odd_list.append(num)
+    numbers[:] = even_list+odd_list
+
+
+def order_even_odd_last_v2(numbers: List[int]) -> None:
+    i, j = 0, len(numbers) - 1
+    while i < j:
+        if numbers[i] % 2 == 0:
+            i += 1
+        else:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            j -= 1
+
+
 if __name__ == '__main__':
-    s = []
-    [s.append(i) for i in range(10)]
-    print(s)
+    l = [1, 4, 3, 3, 3, 3, 36, 9, 8, 4, 5, 6, 7, 8]
+    order_even_odd_last_v2(l)
+    print(l)
